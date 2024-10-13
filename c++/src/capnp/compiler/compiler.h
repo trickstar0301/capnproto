1d2fe2d9f3e68d7413d247bbc6fae2f712d594e8
trickstar <tomida.ldp@gmail.com>
2024-10-13 23:22:34 +0900

    Update c++/src/capnp/compiler/compiler.h
    Co-authored-by: Kenton Varda <kenton@sandstorm.io>

--- c++/src/capnp/compiler/compiler.h
+++ c++/src/capnp/compiler/compiler.h
@@ -61,9 +61,9 @@ public:
 
   virtual kj::Maybe<kj::Array<const byte>> embedRelative(kj::StringPtr embedPath) = 0;
   // Read and return the content of a file specified using `embed`.
-  virtual kj::ArrayPtr<const Resolution> getResolutions(){
-    return kj::ArrayPtr<const Resolution>();
-  };
+
+  virtual kj::ArrayPtr<const Resolution> getResolutions() { return nullptr; }
+  // Get the list of all resolutions reported using ErrorHandler::reportResolution().
 };
 
 class Compiler final: private SchemaLoader::LazyLoadCallback {
